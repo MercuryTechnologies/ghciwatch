7dd0e7904c87f42455446e8d85c5d98111a0cb08
Rebecca Turner <rbt@sent.as>
2023-08-28 18:49:33 -0400

    Add garnix.io for caching and CI (#66)
    https://garnix.io/
    
    Pros:
    * Caching for free
    * Evaluation of checks, packages, devShells, etc. in parallel
    
    Cons:
    * In beta and it shows
    * Logs seem to come a little slow in their web UI
    * When builds finish, the “such and such derivations will be fetched,
    such and such derivations will be built” output is erased, even though I
    just saw it there...
    * Only the last 100 lines of logs are saved (is this the case for
    failures too...?)

--- flake.nix
+++ flake.nix
@@ -20,6 +20,12 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = ["https://cache.garnix.io"];
+    extra-trusted-substituters = ["https://cache.garnix.io"];
+    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
+
   outputs = {
     self,
     nixpkgs,
