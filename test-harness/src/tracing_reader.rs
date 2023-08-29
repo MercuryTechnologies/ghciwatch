deb2f2547b208145971c031b4238a3b586ee5a9a
Rebecca Turner <rbt@sent.as>
2023-08-29 17:37:21 -0400

    Add tracing JSON reader to test-harness (#62)
    The Tracing JSON reader is an async task which will wait for `ghcid-ng`
    to write tracing log events to a file, deserialize them, and make them
    available to the test harness.
    
    This is the core of the tests; `ghcid-ng` is started, we wait for a
    particular log event, we make changes with the IO helpers, repeat.

---
+++ test-harness/src/tracing_reader.rs
@@ -0,0 +1,58 @@
+#![allow(dead_code)]
+
+use std::path::Path;
+use std::time::Duration;
+
+use backoff::backoff::Backoff;
+use backoff::ExponentialBackoff;
+use miette::Context;
+use miette::IntoDiagnostic;
+use tokio::fs::File;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
+use tokio::io::Lines;
+
+use super::Event;
+
+/// A task to read JSON tracing log events output by `ghid-ng` and send them over a channel.
+pub struct TracingReader {
+    lines: Lines<BufReader<File>>,
+}
+
+impl TracingReader {
+    /// Create a new [`TracingReader`].
+    ///
+    /// This watches for data to be read from the given `path`. When a line is written to `path`
+    /// (by `ghcid-ng`), the `TracingReader` will deserialize the line from JSON into an [`Event`]
+    /// and send it to the given `sender` for another task to receive.
+    pub async fn new(path: impl AsRef<Path>) -> miette::Result<Self> {
+        let path = path.as_ref();
+
+        let file = File::open(path)
+            .await
+            .into_diagnostic()
+            .wrap_err_with(|| format!("Failed to open {path:?}"))?;
+
+        let lines = BufReader::new(file).lines();
+
+        Ok(Self { lines })
+    }
+
+    async fn next_event(&mut self) -> miette::Result<Event> {
+        let mut backoff = ExponentialBackoff {
+            max_elapsed_time: None,
+            max_interval: Duration::from_secs(1),
+            ..Default::default()
+        };
+
+        while let Some(duration) = backoff.next_backoff() {
+            if let Some(line) = self.lines.next_line().await.into_diagnostic()? {
+                let event = serde_json::from_str(&line).into_diagnostic()?;
+                return Ok(event);
+            }
+            tokio::time::sleep(duration).await;
+        }
+
+        unreachable!()
+    }
+}
