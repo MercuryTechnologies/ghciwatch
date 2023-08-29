e183f9ab64e3f6b1a1ffcd0f3e83405ebba45bab
Rebecca Turner <rbt@sent.as>
2023-08-29 12:29:52 -0700

    TracingReader doesn't need to be a separate task

--- test-harness/src/tracing_reader.rs
+++ test-harness/src/tracing_reader.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::path::Path;
 use std::time::Duration;
 
@@ -9,15 +11,11 @@ use tokio::fs::File;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::BufReader;
 use tokio::io::Lines;
-use tokio::sync::mpsc;
-use tracing::instrument;
 
 use super::Event;
 
 /// A task to read JSON tracing log events output by `ghid-ng` and send them over a channel.
-#[allow(dead_code)]
 pub struct TracingReader {
-    sender: mpsc::Sender<Event>,
     lines: Lines<BufReader<File>>,
 }
 
@@ -27,7 +25,7 @@ impl TracingReader {
     /// This watches for data to be read from the given `path`. When a line is written to `path`
     /// (by `ghcid-ng`), the `TracingReader` will deserialize the line from JSON into an [`Event`]
     /// and send it to the given `sender` for another task to receive.
-    pub async fn new(sender: mpsc::Sender<Event>, path: impl AsRef<Path>) -> miette::Result<Self> {
+    pub async fn new(path: impl AsRef<Path>) -> miette::Result<Self> {
         let path = path.as_ref();
 
         let file = File::open(path)
@@ -37,29 +35,10 @@ impl TracingReader {
 
         let lines = BufReader::new(file).lines();
 
-        Ok(Self { sender, lines })
+        Ok(Self { lines })
     }
 
-    /// Run this task.
-    #[instrument(skip(self), name = "json-reader", level = "debug")]
-    pub async fn run(mut self) -> miette::Result<()> {
-        loop {
-            match self.run_inner().await {
-                Ok(()) => {
-                    // Graceful shutdown
-                    tracing::debug!("JSON reader exiting");
-                    break;
-                }
-                Err(err) => {
-                    tracing::error!("{err:?}");
-                }
-            }
-        }
-
-        Ok(())
-    }
-
-    async fn run_inner(&mut self) -> miette::Result<()> {
+    async fn next_event(&mut self) -> miette::Result<Event> {
         let mut backoff = ExponentialBackoff {
             max_elapsed_time: None,
             max_interval: Duration::from_secs(1),
@@ -67,13 +46,13 @@ impl TracingReader {
         };
 
         while let Some(duration) = backoff.next_backoff() {
-            while let Some(line) = self.lines.next_line().await.into_diagnostic()? {
+            if let Some(line) = self.lines.next_line().await.into_diagnostic()? {
                 let event = serde_json::from_str(&line).into_diagnostic()?;
-                self.sender.send(event).await.into_diagnostic()?;
+                return Ok(event);
             }
             tokio::time::sleep(duration).await;
         }
 
-        Ok(())
+        unreachable!()
     }
 }
