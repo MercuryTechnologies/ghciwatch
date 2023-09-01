afe9e3a592f3764c5be406e4c22fb3a20330fa27
Rebecca Turner <rbt@sent.as>
2023-09-01 16:48:53 -0700

    Add `--test` integration tests

---
+++ ghcid-ng/tests/test.rs
@@ -0,0 +1,48 @@
+use expect_test::expect;
+use test_harness::fs;
+use test_harness::test;
+use test_harness::GhcidNg;
+use test_harness::Matcher;
+
+/// Test that `ghcid-ng --test ...` can run a test suite.
+#[test]
+async fn can_run_test_suite_on_reload() {
+    let error_path = "ghcid.txt";
+    let mut session = GhcidNg::new_with_args(
+        "tests/data/simple",
+        ["--test", "TestMain.main", "--errors", error_path],
+    )
+    .await
+    .expect("ghcid-ng starts");
+    let error_path = session.path(error_path);
+    session
+        .wait_until_ready()
+        .await
+        .expect("ghcid-ng loads ghci");
+    fs::touch(session.path("src/MyLib.hs"))
+        .await
+        .expect("Can touch file");
+    session
+        .get_log("Finished running tests")
+        .await
+        .expect("ghcid-ng runs the test suite");
+
+    session
+        .get_log(
+            Matcher::span_close()
+                .in_span("write")
+                .in_module("ghcid_ng::ghci::stderr"),
+        )
+        .await
+        .expect("ghcid-ng writes ghcid.txt");
+
+    let error_contents = fs::read(&error_path)
+        .await
+        .expect("ghcid-ng writes ghcid.txt");
+
+    expect![[r#"
+        Ok, three modules loaded.
+        0 tests executed, 0 failures :)
+    "#]]
+    .assert_eq(&error_contents);
+}
--- test-harness/src/fs.rs
+++ test-harness/src/fs.rs
@@ -17,17 +17,24 @@ use tokio::io::AsyncWriteExt;
 #[tracing::instrument]
 pub async fn touch(path: impl AsRef<Path> + Debug) -> miette::Result<()> {
     let path = path.as_ref();
-    if let Some(parent) = path.parent() {
-        create_dir(parent).await?;
+    if path.exists() {
+        // I've had trouble with the pure-`open` approach getting detected, so let's actually
+        // write the file's contents again.
+        let contents = read(path).await?;
+        write(path, contents).await
+    } else {
+        if let Some(parent) = path.parent() {
+            create_dir(parent).await?;
+        }
+        OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(path)
+            .await
+            .into_diagnostic()
+            .wrap_err_with(|| format!("Failed to touch {path:?}"))
+            .map(|_| ())
     }
-    OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(path)
-        .await
-        .into_diagnostic()
-        .wrap_err_with(|| format!("Failed to touch {path:?}"))
-        .map(|_| ())
 }
 
 /// Write some data to a path, replacing its previous contents.
