6e7d5a5d10ab0e44b53907b906b7604477e46a7f
Rebecca Turner <rbt@sent.as>
2023-09-06 12:29:39 -0700

    Remove `TestMain.main`

--- ghcid-ng/tests/data/simple/test/TestMain.hs
+++ ghcid-ng/tests/data/simple/test/TestMain.hs
@@ -1,9 +1,6 @@
-module TestMain (main, testMain) where
+module TestMain (testMain) where
 
 import System.IO (hPutStrLn, stderr)
-
-main :: IO ()
-main = testMain
 
 testMain :: IO ()
 testMain = hPutStrLn stderr "0 tests executed, 0 failures :)"
