cb5c61b2712fd4fc1fa4a727b5adbf8f7a3ff07f
Rebecca Turner <rbt@sent.as>
2023-09-21 12:56:44 -0700

    Remove `hsboot` extension

--- src/haskell_source_file.rs
+++ src/haskell_source_file.rs
@@ -3,11 +3,10 @@
 use camino::Utf8Path;
 
 /// File extensions for Haskell source code.
-pub const HASKELL_SOURCE_EXTENSIONS: [&str; 9] = [
+pub const HASKELL_SOURCE_EXTENSIONS: [&str; 8] = [
     // NOTE: This should start with `hs` so that iterators try the most common extension first.
     "hs",      // Haskell
     "lhs",     // Literate Haskell
-    "hsboot",  // Haskell boot file.
     "hs-boot", // See: https://downloads.haskell.org/ghc/latest/docs/users_guide/separate_compilation.html#how-to-compile-mutually-recursive-modules
     "hsc", // `hsc2hs` C bindings: https://downloads.haskell.org/ghc/latest/docs/users_guide/utils.html?highlight=interfaces#writing-haskell-interfaces-to-c-code-hsc2hs
     "x",   // `alex` (lexer generator): https://hackage.haskell.org/package/alex
