5f8e8b1028e27b57938a13fc31ce2828f46bfded
Rebecca Turner <rbt@sent.as>
2023-09-28 12:44:47 -0700

    Housekeeping: Move `clap` parsers into relevant modules (#109)
    It's weird to have these in a separate part of the tree than where the
    types they parse are defined.

--- src/clap/mod.rs
+++ src/clap/mod.rs
@@ -4,15 +4,11 @@ mod camino;
 mod clonable_command;
 mod error_message;
 mod fmt_span;
-mod ghci_command;
 mod humantime;
-mod normal_path;
 mod rust_backtrace;
 
 pub use self::humantime::DurationValueParser;
 pub use clonable_command::ClonableCommandParser;
 pub use error_message::value_validation_error;
 pub use fmt_span::FmtSpanParserFactory;
-pub use ghci_command::GhciCommandParser;
-pub use normal_path::NormalPathValueParser;
 pub use rust_backtrace::RustBacktrace;
--- src/normal_path.rs
+++ src/normal_path.rs
@@ -9,6 +9,9 @@ use std::path::Path;
 
 use camino::Utf8Path;
 use camino::Utf8PathBuf;
+use clap::builder::PathBufValueParser;
+use clap::builder::TypedValueParser;
+use clap::builder::ValueParserFactory;
 use miette::miette;
 use miette::Context;
 use miette::IntoDiagnostic;
@@ -137,5 +140,40 @@ impl Deref for NormalPath {
 
     fn deref(&self) -> &Self::Target {
         &self.normal
+    }
+}
+
+/// [`clap`] parser for [`NormalPath`] values.
+#[derive(Default, Clone)]
+pub struct NormalPathValueParser {
+    inner: PathBufValueParser,
+}
+
+impl TypedValueParser for NormalPathValueParser {
+    type Value = NormalPath;
+
+    fn parse_ref(
+        &self,
+        cmd: &clap::Command,
+        arg: Option<&clap::Arg>,
+        value: &std::ffi::OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        self.inner.parse_ref(cmd, arg, value).and_then(|path_buf| {
+            NormalPath::from_cwd(path_buf).map_err(|err| {
+                crate::clap::value_validation_error(
+                    arg,
+                    &value.to_string_lossy(),
+                    format!("{err:?}"),
+                )
+            })
+        })
+    }
+}
+
+impl ValueParserFactory for NormalPath {
+    type Parser = NormalPathValueParser;
+
+    fn value_parser() -> Self::Parser {
+        Self::Parser::default()
     }
 }
