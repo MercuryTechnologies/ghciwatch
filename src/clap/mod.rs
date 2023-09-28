30a7a5a0d8be0007fa4b0cddf1395344d5b91195
Rebecca Turner <rbt@sent.as>
2023-09-28 11:21:34 -0700

    Move `ClonableCommand` to a separate module

--- src/clap/mod.rs
+++ src/clap/mod.rs
@@ -1,14 +1,12 @@
 //! Adapters for parsing [`clap`] arguments to various types.
 
 mod camino;
-mod clonable_command;
 mod error_message;
 mod fmt_span;
 mod humantime;
 mod rust_backtrace;
 
 pub use self::humantime::DurationValueParser;
-pub use clonable_command::ClonableCommandParser;
 pub use error_message::value_validation_error;
 pub use fmt_span::FmtSpanParserFactory;
 pub use rust_backtrace::RustBacktrace;
