b230a323ae14c3e03e9fc4fedd5a416bd33e6aa7
Evan Relf <evan@evanrelf.com>
2023-12-01 17:08:35 -0800

    Handle `crossterm` events

--- src/tui.rs
+++ src/tui.rs
@@ -1,6 +1,8 @@
 use crate::{ghci::manager::GhciEvent, terminal, ShutdownHandle};
-use miette::WrapErr as _;
+use crossterm::event::{Event, EventStream, KeyCode, KeyModifiers};
+use miette::{miette, IntoDiagnostic as _, WrapErr as _};
 use tokio::sync::mpsc;
+use tokio_stream::StreamExt as _;
 
 /// TODO(evan): Document
 pub async fn run_tui(
@@ -9,5 +11,37 @@ pub async fn run_tui(
 ) -> miette::Result<()> {
     let mut _terminal = terminal::enter().wrap_err("Failed to enter terminal")?;
 
+    let mut event_stream = EventStream::new();
+
+    loop {
+        let event = event_stream
+            .next()
+            .await
+            .ok_or(miette!("No more crossterm events"))?
+            .into_diagnostic()
+            .wrap_err("Failed to get next crossterm event")?;
+
+        let quit = handle_event(event);
+
+        if quit {
+            break;
+        }
+    }
+
     Ok(())
+}
+
+fn handle_event(event: Event) -> bool {
+    let mut quit = false;
+
+    match event {
+        Event::Key(key)
+            if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') =>
+        {
+            quit = true;
+        }
+        _ => {}
+    }
+
+    quit
 }
