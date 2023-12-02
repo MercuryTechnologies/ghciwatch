1b0ae106e63b6a4397d91fafa1009d17162796aa
Evan Relf <evan@evanrelf.com>
2023-12-01 16:53:45 -0800

    Enter terminal

--- src/tui.rs
+++ src/tui.rs
@@ -1,4 +1,5 @@
-use crate::{ghci::manager::GhciEvent, ShutdownHandle};
+use crate::{ghci::manager::GhciEvent, terminal, ShutdownHandle};
+use miette::WrapErr as _;
 use tokio::sync::mpsc;
 
 /// TODO(evan): Document
@@ -6,5 +7,7 @@ pub async fn run_tui(
     _handle: ShutdownHandle,
     _ghci_sender: mpsc::Sender<GhciEvent>,
 ) -> miette::Result<()> {
+    let mut _terminal = terminal::enter().wrap_err("Failed to enter terminal")?;
+
     Ok(())
 }
