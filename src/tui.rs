b59696d492606dca9fd5157b18cb91d6ac555465
Evan Relf <evan@evanrelf.com>
2023-12-07 13:07:05 -0800

    Render "hello world"

--- src/tui.rs
+++ src/tui.rs
@@ -7,6 +7,10 @@ use crossterm::event::KeyModifiers;
 use miette::miette;
 use miette::IntoDiagnostic;
 use miette::WrapErr;
+use ratatui::prelude::Buffer;
+use ratatui::prelude::Rect;
+use ratatui::widgets::Paragraph;
+use ratatui::widgets::Widget;
 use tokio_stream::StreamExt;
 
 #[derive(Default)]
@@ -16,13 +20,22 @@ struct Tui {
 
 /// TODO(evan): Document
 pub async fn run_tui(mut shutdown: ShutdownHandle) -> miette::Result<()> {
-    let _terminal = terminal::enter()?;
+    let mut terminal = terminal::enter()?;
 
     let mut tui = Tui::default();
 
     let mut event_stream = EventStream::new();
 
     while !tui.quit {
+        terminal
+            .draw(|frame| {
+                let area = frame.size();
+                let buffer = frame.buffer_mut();
+                render(&tui, area, buffer);
+            })
+            .into_diagnostic()
+            .wrap_err("Failed to draw to terminal")?;
+
         tokio::select! {
             _ = shutdown.on_shutdown_requested() => {
                 tui.quit = true;
@@ -40,6 +53,10 @@ pub async fn run_tui(mut shutdown: ShutdownHandle) -> miette::Result<()> {
     let _ = shutdown.request_shutdown();
 
     Ok(())
+}
+
+fn render(_tui: &Tui, area: Rect, buffer: &mut Buffer) {
+    Paragraph::new("Hello, world!").render(area, buffer);
 }
 
 fn handle_event(tui: &mut Tui, event: Event) -> miette::Result<()> {
