4dd78ed2354ccd654c60396100268fac06c5d890
Evan Relf <evan@evanrelf.com>
2023-12-07 15:44:48 -0800

    Hide enum variants
    Makes the correct usage more obvious, removes the need to document the
    variants.

--- src/ghci/writer.rs
+++ src/ghci/writer.rs
@@ -17,36 +17,37 @@ use tokio_util::compat::TokioAsyncWriteCompatExt;
 /// A dynamically reconfigurable sink for `ghci` process output. Built for use in `GhciOpts`, but
 /// usable as a general purpose clonable [`AsyncWrite`]r.
 #[derive(Debug)]
-pub enum GhciWriter {
-    /// Write to `stdout`.
+pub struct GhciWriter(Kind);
+
+#[derive(Debug)]
+enum Kind {
     Stdout(Stdout),
-    /// Write to `stderr`.
     Stderr(Stderr),
-    /// Write to an in-memory buffer.
     DuplexStream(Compat<Arc<Mutex<Compat<DuplexStream>>>>),
-    /// Write to the void.
     Sink(Sink),
 }
 
 impl GhciWriter {
     /// Write to `stdout`.
     pub fn stdout() -> Self {
-        Self::Stdout(tokio::io::stdout())
+        Self(Kind::Stdout(tokio::io::stdout()))
     }
 
     /// Write to `stderr`.
     pub fn stderr() -> Self {
-        Self::Stderr(tokio::io::stderr())
+        Self(Kind::Stderr(tokio::io::stderr()))
     }
 
     /// Write to an in-memory buffer.
     pub fn duplex_stream(duplex_stream: DuplexStream) -> Self {
-        Self::DuplexStream(Arc::new(Mutex::new(duplex_stream.compat_write())).compat_write())
+        Self(Kind::DuplexStream(
+            Arc::new(Mutex::new(duplex_stream.compat_write())).compat_write(),
+        ))
     }
 
     /// Write to the void.
     pub fn sink() -> Self {
-        Self::Sink(tokio::io::sink())
+        Self(Kind::Sink(tokio::io::sink()))
     }
 }
 
@@ -56,40 +57,40 @@ impl AsyncWrite for GhciWriter {
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, io::Error>> {
-        match Pin::into_inner(self) {
-            Self::Stdout(ref mut x) => Pin::new(x).poll_write(cx, buf),
-            Self::Stderr(ref mut x) => Pin::new(x).poll_write(cx, buf),
-            Self::DuplexStream(ref mut x) => Pin::new(x).poll_write(cx, buf),
-            Self::Sink(ref mut x) => Pin::new(x).poll_write(cx, buf),
+        match Pin::into_inner(self).0 {
+            Kind::Stdout(ref mut x) => Pin::new(x).poll_write(cx, buf),
+            Kind::Stderr(ref mut x) => Pin::new(x).poll_write(cx, buf),
+            Kind::DuplexStream(ref mut x) => Pin::new(x).poll_write(cx, buf),
+            Kind::Sink(ref mut x) => Pin::new(x).poll_write(cx, buf),
         }
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        match Pin::into_inner(self) {
-            Self::Stdout(ref mut x) => Pin::new(x).poll_flush(cx),
-            Self::Stderr(ref mut x) => Pin::new(x).poll_flush(cx),
-            Self::DuplexStream(ref mut x) => Pin::new(x).poll_flush(cx),
-            Self::Sink(ref mut x) => Pin::new(x).poll_flush(cx),
+        match Pin::into_inner(self).0 {
+            Kind::Stdout(ref mut x) => Pin::new(x).poll_flush(cx),
+            Kind::Stderr(ref mut x) => Pin::new(x).poll_flush(cx),
+            Kind::DuplexStream(ref mut x) => Pin::new(x).poll_flush(cx),
+            Kind::Sink(ref mut x) => Pin::new(x).poll_flush(cx),
         }
     }
 
     fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        match Pin::into_inner(self) {
-            Self::Stdout(ref mut x) => Pin::new(x).poll_shutdown(cx),
-            Self::Stderr(ref mut x) => Pin::new(x).poll_shutdown(cx),
-            Self::DuplexStream(ref mut x) => Pin::new(x).poll_shutdown(cx),
-            Self::Sink(ref mut x) => Pin::new(x).poll_shutdown(cx),
+        match Pin::into_inner(self).0 {
+            Kind::Stdout(ref mut x) => Pin::new(x).poll_shutdown(cx),
+            Kind::Stderr(ref mut x) => Pin::new(x).poll_shutdown(cx),
+            Kind::DuplexStream(ref mut x) => Pin::new(x).poll_shutdown(cx),
+            Kind::Sink(ref mut x) => Pin::new(x).poll_shutdown(cx),
         }
     }
 }
 
 impl Clone for GhciWriter {
     fn clone(&self) -> Self {
-        match self {
-            Self::Stdout(_) => Self::stdout(),
-            Self::Stderr(_) => Self::stderr(),
-            Self::DuplexStream(x) => Self::DuplexStream(x.clone()),
-            Self::Sink(_) => Self::sink(),
+        match &self.0 {
+            Kind::Stdout(_) => Self::stdout(),
+            Kind::Stderr(_) => Self::stderr(),
+            Kind::DuplexStream(x) => Self(Kind::DuplexStream(x.clone())),
+            Kind::Sink(_) => Self::sink(),
         }
     }
 }
