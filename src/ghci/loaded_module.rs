ffde01730e6e2bb362fae7fcfa5723fc94bb62bd
Rebecca Turner <rbt@sent.as>
2024-06-12 13:00:29 -0700

    Remove `LoadedModuleName`

--- src/ghci/loaded_module.rs
+++ src/ghci/loaded_module.rs
@@ -12,10 +12,10 @@ use crate::normal_path::NormalPath;
 /// Hashing and equality are determined by the module's path alone.
 #[derive(Debug, Clone, Eq)]
 pub struct LoadedModule {
-    /// The module's source file.
+    /// The module's source file, like `src/My/Cool/Module.hs`.
     path: NormalPath,
 
-    /// The module's name.
+    /// The module's dotted name, like `My.Cool.Module`.
     ///
     /// This is present if and only if the module is loaded by name.
     ///
@@ -41,14 +41,6 @@ impl LoadedModule {
         }
     }
 
-    /// Get the name to use to refer to this module.
-    pub fn name(&self) -> LoadedModuleName {
-        match self.name.as_deref() {
-            Some(name) => LoadedModuleName::Name(name),
-            None => LoadedModuleName::Path(&self.path),
-        }
-    }
-
     /// Get the module's source path.
     pub fn path(&self) -> &NormalPath {
         &self.path
@@ -57,7 +49,13 @@ impl LoadedModule {
 
 impl Display for LoadedModule {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.name())
+        write!(
+            f,
+            "{}",
+            self.name
+                .as_deref()
+                .unwrap_or_else(|| self.path.relative().as_str())
+        )
     }
 }
 
@@ -94,29 +92,5 @@ impl Borrow<NormalPath> for LoadedModule {
 impl Borrow<Utf8Path> for LoadedModule {
     fn borrow(&self) -> &Utf8Path {
         &self.path
-    }
-}
-
-/// The name to use to refer to a module loaded into a GHCi session.
-///
-/// Entries in `:show targets` can be one of two types: module paths or module names (with `.` in
-/// place of path separators). Due to a `ghci` bug, the module can only be referred to as whichever
-/// form it was originally added as (see below), so we use this to track how we refer to modules.
-///
-/// See: <https://gitlab.haskell.org/ghc/ghc/-/issues/13254#note_525037>
-#[derive(Debug)]
-pub enum LoadedModuleName<'a> {
-    /// A path to a Haskell source file, like `src/My/Cool/Module.hs`.
-    Path(&'a Utf8Path),
-    /// A dotted module name, like `My.Cool.Module`.
-    Name(&'a str),
-}
-
-impl<'a> Display for LoadedModuleName<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            LoadedModuleName::Path(path) => write!(f, "{path}"),
-            LoadedModuleName::Name(name) => write!(f, "{name}"),
-        }
     }
 }
